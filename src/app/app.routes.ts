40c32e94a682c0dccae08e4eb69f7675f9204ed1
codewat <cemar.watin@triosstudent.com>
2026-04-07 19:24:55 -0400

    added component add-item

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { AddItem } from './Components/add-item/add-item'; 
+import { ItemList } from './Components/item-list/item-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { 
+    path: 'add-item', 
+    component: AddItem
+  },
+  { 
+    path: 'items', 
+    component: ItemList
+  },
+  { 
+    path: '', 
+    redirectTo: 'items', 
+    pathMatch: 'full' 
+  }
+];

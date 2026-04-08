1f70fa3025a80bb604612591169939f6fbc543f6
StrugglingAce <chibuzor.awanye@triosstudent.com>
2026-04-08 19:01:58 -0400

    Item Card component

--- src/app/Components/item-card/item-card.ts
+++ src/app/Components/item-card/item-card.ts
@@ -2,6 +2,7 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core'; 
 import { CommonModule } from '@angular/common';
 import { BadgeComponent } from '../badge/badge';
+import { Report } from '../../models/report.model';
 
 @Component({
   selector: 'app-item-card',
@@ -12,7 +13,7 @@ import { BadgeComponent } from '../badge/badge';
 })
 export class ItemCardComponent {
   // 2. Make sure this exact line is inside the class!
-  @Input({ required: true }) item!: LostItem; 
+  @Input({ required: true }) item!: Report; 
 
   @Output() toggleStatus = new EventEmitter<string>();
 
